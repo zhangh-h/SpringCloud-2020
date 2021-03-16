14f9bb643c817d7cfa564be7cfb6d0ec86d599ec
xiao张 <zhangheng1525@qq.com>
2021-03-16 18:03:54 +0800

    新增nacos依赖以及配置

--- cloud-provider-payment-8001/src/main/java/com/xiaozhang/springcloud/Payment8001.java
+++ cloud-provider-payment-8001/src/main/java/com/xiaozhang/springcloud/Payment8001.java
@@ -3,7 +3,6 @@ package com.xiaozhang.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @auther: ZhangH
@@ -11,7 +10,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @version: 1.0
  */
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient  //服务发现  用于发现服务eureka  zookeeper   consul ，后面可以直接使用这个注解。
 public class Payment8001 {
     public static void main(String[] args) {
